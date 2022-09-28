e6765803c0d824aaedcff2ea1b5b524cf519f8b2
Thomas Toulouse <2134063@cegepba.qc.ca>
2022-09-27 21:58:40 -0700

    supprimer les commentaire inutile

--- Travail1/Controls/UserInfoJoueur.cs
+++ Travail1/Controls/UserInfoJoueur.cs
@@ -67,48 +67,7 @@ namespace Travail1.Controls
             lbAffichageJoueur1.Text = controleur.Joueurs[1].Nom;
         }
 
-        /* private void SetDe(De? de)
-         {
-             if (de is null)
-             {
-                 lblAffichageTypeDe.Text = "";
-                 lblAffichageValeurDe.Text = "";
-             }
-             else
-             {
-                 Desabonner(); // désabonne de l'ancien dé
-             }
-             this.de = de;
-             if (de is not null)
-             {
-                 InitAffichage();
-                 Abonner(); // abonne au nouveau dé
-             }
-         }
-
-         private void Desabonner()
-         {
-             if (de is not null)
-             {
-                 de.ValeurChanged -= De_ValeurChanged;
-             }
-         }
-
-         private void Abonner()
-         {
-             de.ValeurChanged += De_ValeurChanged;
-         }
-
-         private void De_ValeurChanged(object? sender, int valeur)
-         {
-             lblAffichageValeurDe.Text = valeur.ToString();
-         }
-
-         private void InitAffichage()
-         {
-             lblAffichageTypeDe.Text = de.TypeDe();
-             lblAffichageValeurDe.Text = de.Valeur.ToString();
-         }*/
+     
 
         private void UserInfoJoueur_Load(object sender, EventArgs e)
         {
