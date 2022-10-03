ca24f9ba7f67f8993ec58f009f7d61cd28262173
Thomas-Toulouse-Cegep <2134063@cegepba.qc.ca>
2022-10-03 12:09:25 -0400

    petit erreur de syntaxe

---
+++ Travail1/Models/Case/CaseTrappe.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Travail1.Models.Point;
+
+namespace Travail1.Models.Case
+{
+    internal class CaseTrappe : Case
+    {
+        public CaseTrappe(Points points, int position) : base(points, position)
+        {
+        }
+
+        private PointF ObtenirCoordonees()
+        {
+            int y = (7 - (position / 8));
+            int x = (position % 8);
+            if ((position / 8) % 2 == 1)
+            {
+                x = 7 - x;
+            }
+            return new PointF { X = x * largeur, Y = y * largeur };
+        }
+
+        public override void Dessiner(Graphics graphics)
+        {
+            var coordonees = ObtenirCoordonees();
+            var font = new Font("Calibri", 20);
+            graphics.FillRectangle(Brushes.Green, coordonees.X, coordonees.Y, largeur, largeur);
+            graphics.DrawString((position + 1).ToString(), font, Brushes.Black, coordonees.X + 30, coordonees.Y + 30);
+        }
+    }
+}
